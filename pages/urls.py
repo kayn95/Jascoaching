f8d8203c203a4ae9f500e78de1b1ffdac6456874
kayn95 <153620921+kayn95@users.noreply.github.com>
2026-01-25 17:46:36 +0100

    changements divers 25.01.2026

--- pages/urls.py
+++ pages/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("services/", views.offers, name="offers"),
-    path("stories/", views.results, name="results"),
-    path("about/", views.about, name="about"),
+    #path("stories/", views.results, name="results"),
+    #path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
 ]
